b9f455b75152821f621de18fa84e4db13cdf6a8a
Andrii Tarykin <ksbomj@gmail.com>
2023-02-21 22:38:00 +0200

    add CNME file

--- deployment/gh-pages-deploy.js
+++ deployment/gh-pages-deploy.js
@@ -9,6 +9,9 @@ const fs = require("fs");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await fs.writeFile(folderName + "/CNAME", "keep.obada.io", (err) => {
+        if (err) throw err;
+    });
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
